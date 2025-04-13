4557de5f74e519d9ae66bed93da49825efdbfcf5
tawashen <tawashen@gmail.com>
2025-04-13 23:02:49 +0900

    残り歩数表示OK

--- main.lua
+++ main.lua
@@ -1,6 +1,33 @@
 local Timer = require 'hump.timer'
 
 
+--メモライズ関係----------------------------------------------------------------------------------------
+
+-- メモファイルのパス
+local MEMO_FILE = "path_memo.lua"
+
+-- メモデータの読み込み
+local function load_memo()
+    local memo = {}
+    if io.open(MEMO_FILE, "r") then
+        memo = dofile(MEMO_FILE) -- ファイルをLuaテーブルとして実行
+    end
+    return memo or {}
+end
+
+-- メモデータの保存
+local function save_memo(memo)
+    local file = io.open(MEMO_FILE, "w")
+    file:write("return {\n")
+    for k, v in pairs(memo) do
+        file:write(string.format("  [%q] = %d,\n", k, v))
+    end
+    file:write("}\n")
+    file:close()
+end
+
+----------------------------------------------------------------------------------------
+
 
 function set_screen_size()
   width = 1000                         -- ゲーム画面の横幅
@@ -108,7 +135,7 @@ C_marusa = {name = "マルサカード", attribute = "マルサ", price = 5000, 
     dice_go = false, dice_num = 1, move_on = false, month = 4,
     move_num = 0, walk_table = {}, 
     at_destination = false, --目的地到達フラグ
-    destination_index = nil, --目的地インデックス
+    destination_index = 51, --目的地インデックス
     message_disp_flag = false, --画面にメッセージを表示するフラグ
     arrival_flag = false,
     pending_dice_roll = nil
@@ -358,15 +385,23 @@ function love.draw()
  if FLAGS.move_on == true then
 
       love.graphics.setColor(0, 0, 0) 
-      love.graphics.rectangle("fill", 800, 120, 200, 180, 10)
+      love.graphics.rectangle("fill", 800, 120, 200, 60, 10)
+      love.graphics.rectangle("fill", 800, 200, 200, 60, 10)
       love.graphics.setLineWidth(5)--枠線の太さ
       love.graphics.setColor(1, 1, 1)
-      love.graphics.rectangle("line", 800, 120, 196, 176, 10) --角を丸める
+      love.graphics.rectangle("line", 800, 120, 196, 54, 10) --角を丸める
+      love.graphics.rectangle("line", 800, 200, 196, 54, 10)
       love.graphics.setLineWidth(1)--枠線の太さ
 
     love.graphics.print(
-      string.format("残り: %02d歩", FLAGS.move_num),
-      810, 130)
+      string.format("残り歩数:%02d歩", FLAGS.move_num),
+      810, 134)
+    love.graphics.print(
+      string.format("目的地迄:%02d歩",
+      nextStation(PLAYERS[FLAGS.currentP].coordinate,
+        FLAGS.destination_index,
+        {}, 0)),
+      810, 214)
     --デバッグ用インデックス表示
     --love.graphics.print(FLAGS.walk_table, 810, 160)
     --if #FLAGS.walk_table > 0 then 
@@ -547,7 +582,81 @@ end
 
 
 
--- created original functions --
+-- created original functions -----------------------------------------------------------------------------
+
+function nextStation(index, target, from_table, count) --> number（最短距離）
+
+local result = {}
+local final = 0
+local memo = load_memo()
+
+local key = string.format("%d_%d", index, target)
+
+if memo[key] then
+    final = memo[key]
+else
+    local function saiki(indexS, from_tableS, countS)
+        local old_target = nextTree(indexS)
+        local new_target = {}
+    for _, targ in pairs(old_target) do --New_targetに履歴にあるコマ以外を追加する
+        if not containsT(from_tableS, targ) then
+            --print(targ)
+            table.insert(new_target, targ)
+        end
+    end
+    for _, nextS in ipairs(new_target) do
+        if nextS == target then
+            new_count = countS + 1
+            print(new_count)
+            table.insert(result, new_count)
+        else 
+            print(nextS)
+            new_from_table = from_tableS
+            table.insert(new_from_table, indexS)
+            new_count = countS + 1
+            saiki(nextS, new_from_table, new_count)
+        end
+    end
+  end
+    saiki(index, from_table, count)
+    final = find_min(result)
+    memo[key] = final
+    save_memo(memo)
+end
+    return final --数値のみを返す
+end
+
+
+function containsT(t, num)
+    for _, num1 in ipairs(t) do
+            if num1 == num then
+                return true
+            end
+        end
+    return false
+end
+
+function find_min(t)
+    local result = 1000000
+    for _, num in pairs(t) do
+        if result > num then
+        result = num
+        end
+    end
+        return result
+end
+
+DIRECT = {-20, 1, 20, -1}
+ -- 1 or 8 raoad
+function nextTree(index) --tree作成
+    local new_index = {}
+    for _, direc in ipairs(DIRECT) do
+    if map[index + direc] == 1 or map[index + direc] == 8 then
+        table.insert(new_index, index + direc * 2) --道を挟んだパネルの座標を追加
+        end
+    end
+    return new_index
+end
 
 
 
--- test.lua
+++ test.lua
@@ -17,55 +17,11 @@ map = {
   }
 
 
---メモライズ関係---
-
--- メモファイルのパス
-local MEMO_FILE = "path_memo.lua"
-
--- メモデータの読み込み
-local function load_memo()
-    local memo = {}
-    if io.open(MEMO_FILE, "r") then
-        memo = dofile(MEMO_FILE) -- ファイルをLuaテーブルとして実行
-    end
-    return memo or {}
-end
-
--- メモデータの保存
-local function save_memo(memo)
-    local file = io.open(MEMO_FILE, "w")
-    file:write("return {\n")
-    for k, v in pairs(memo) do
-        file:write(string.format("  [%q] = %d,\n", k, v))
-    end
-    file:write("}\n")
-    file:close()
-end
 
 -- 使用例
 
 
-function find_min(t)
-    local result = 1000000
-    for _, num in pairs(t) do
-        if result > num then
-        result = num
-        end
-    end
-        return result
-end
 
-DIRECT = {-20, 1, 20, -1}
- -- 1 or 8 raoad
-function nextTree(index) --tree作成
-    local new_index = {}
-    for _, direc in ipairs(DIRECT) do
-    if map[index + direc] == 1 or map[index + direc] == 8 then
-        table.insert(new_index, index + direc * 2) --道を挟んだパネルの座標を追加
-        end
-    end
-    return new_index
-end
 
 --isStation(43)
 
@@ -79,61 +35,13 @@ function tableToString(t)
 end
 
 
-function containsT(t, num)
-    for _, num1 in ipairs(t) do
-            if num1 == num then
-                return true
-            end
-        end
-    return false
-end
+
 
 
 
  
 
 
-function nextStation(index, target, from_table, count) --> number（最短距離）
-
-local result = {}
-local final = nil
-local memo = load_memo()
-
-local key = string.format("%d_%d", index, target)
-
-if memo[key] then
-    final = memo[key]
-else
-    local function saiki(indexS, from_tableS, countS)
-        local old_target = nextTree(indexS)
-        local new_target = {}
-    for _, targ in pairs(old_target) do --New_targetに履歴にあるコマ以外を追加する
-        if not containsT(from_tableS, targ) then
-            --print(targ)
-            table.insert(new_target, targ)
-        end
-    end
-    for _, nextS in ipairs(new_target) do
-        if nextS == target then
-            new_count = countS + 1
-            print(new_count)
-            table.insert(result, new_count)
-        else 
-            print(nextS)
-            new_from_table = from_tableS
-            table.insert(new_from_table, indexS)
-            new_count = countS + 1
-            saiki(nextS, new_from_table, new_count)
-        end
-    end
-  end
-    saiki(index, from_table, count)
-    final = find_min(result)
-    memo[key] = final
-    save_memo(memo)
-end
-    return final --数値のみを返す
-end
 
 
 
